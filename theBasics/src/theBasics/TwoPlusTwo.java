785fdc50599e37c40d0c7ec3bc2fd147a6f6452b
wolves621 <cats1262@gmail.com>
2014-06-07 13:19:48 -0500

    Quiz#1

---
+++ theBasics/src/theBasics/TwoPlusTwo.java
@@ -0,0 +1,25 @@
+package theBasics;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class TwoPlusTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=3;
+		
+		int y=2;
+		int z=(x + y);
+		System.out.print(x+"+"+y+"="+(x+y));
+		//System.out.print("+");
+		//System.out.print(y);
+		//System.out.print("=");
+		//System.out.print(z);
+		System.out.print("\nUncle Tim is awesome!");
+		
+		
+			
+	}
+
+}
