9887b5f75cb589bb8e04f11943a6e249cafd5739
wolves621 <cats1262@gmail.com>
2014-10-12 12:13:12 -0500

    hi

--- theBasics/src/theBasics/TwoPlusTwo.java
+++ theBasics/src/theBasics/TwoPlusTwo.java
@@ -1,21 +1,20 @@
 package theBasics;
 
-import java.io.DataInputStream;
-import java.io.IOException;
+//import java.io.DataInputStream;
+//import java.io.IOException;
 
 public class TwoPlusTwo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x=3;
+		int x=545;
 		
-		int y=2;
-		int z=(x + y);
-		System.out.print(x+"+"+y+"="+(x+y));
-		//System.out.print("+");
-		//System.out.print(y);
-		//System.out.print("=");
-		//System.out.print(z);
+		int y=546;
+		int z=(x - y);
+		if (z>4)
+		System.out.print(x+"-"+y+"=");
+		System.out.print(z);
+		
 		System.out.print("\nUncle Tim is awesome!");
 		
 		
